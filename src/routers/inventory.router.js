6b62afefe3e317007f79739fc6989b302c6c3a7b
levandat <lvd.levandat@gmail.com>
2023-04-11 04:26:30 +0700

    Get inbound 🙂

--- src/routers/inventory.router.js
+++ src/routers/inventory.router.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {createInbound, outbound} = require('../controller/inventory.ctrl');
-const {checkAuthorizationAdmin} = require('../middleware/auth.middleware')
+const {
+        createInbound, 
+        outbound, 
+        getAllInBound,
+        getAllInBoundCancle,
+        getAllInBoundCompleted,
+        getAllInBoundPending,
+        getAllInBoundProcessing,
+        getAllInBoundUnPaid,
+        getAllInBoundPaid
+    } = require('../controller/inventory.ctrl');
+const {checkAuthorizationAdmin} = require('../middleware/auth.middleware');
+
 let initInventoryRouter = (app) => {
     router.post('/inbound', checkAuthorizationAdmin, createInbound);
-    // router.post('/update', checkAuthorizationAdmin,updateCategory);
-    // router.get('/categories', checkAuthorizationAdmin, getAllCategory);
-    // router.get('/categories-active', getAllCategoryActive);
-    // router.get('/:categorySlug', getAllProductsByCategory);
+    router.get('/inbound', checkAuthorizationAdmin, getAllInBound);
+    router.get('/inbound-cplt', checkAuthorizationAdmin, getAllInBoundCompleted);
+    router.get('/inbound-pending', checkAuthorizationAdmin, getAllInBoundPending);
+    router.get('/inbound-processing', checkAuthorizationAdmin, getAllInBoundProcessing);
+    router.get('/inbound-unpaid', checkAuthorizationAdmin, getAllInBoundUnPaid);
+    router.get('/inbound-paid', checkAuthorizationAdmin, getAllInBoundPaid);
+    router.get('/inbound-cancle', checkAuthorizationAdmin, getAllInBoundCancle);
     app.use("/api/inventory",router);
 }
 
