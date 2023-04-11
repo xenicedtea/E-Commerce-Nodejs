340300269e1c272a1ddeb9ca7b5253f1af2075e3
levandat <lvd.levandat@gmail.com>
2023-04-11 20:32:36 +0700

    update inbound

--- src/routers/inventory.router.js
+++ src/routers/inventory.router.js
@@ -4,24 +4,14 @@ const {
         createInbound, 
         outbound, 
         getAllInBound,
-        getAllInBoundCancle,
-        getAllInBoundCompleted,
-        getAllInBoundPending,
-        getAllInBoundProcessing,
-        getAllInBoundUnPaid,
-        getAllInBoundPaid
+        getAllInBoundStatus,
     } = require('../controller/inventory.ctrl');
 const {checkAuthorizationAdmin} = require('../middleware/auth.middleware');
 
 let initInventoryRouter = (app) => {
     router.post('/inbound', checkAuthorizationAdmin, createInbound);
+    router.get('/inbound/:status', checkAuthorizationAdmin, getAllInBoundStatus);
     router.get('/inbound', checkAuthorizationAdmin, getAllInBound);
-    router.get('/inbound-cplt', checkAuthorizationAdmin, getAllInBoundCompleted);
-    router.get('/inbound-pending', checkAuthorizationAdmin, getAllInBoundPending);
-    router.get('/inbound-processing', checkAuthorizationAdmin, getAllInBoundProcessing);
-    router.get('/inbound-unpaid', checkAuthorizationAdmin, getAllInBoundUnPaid);
-    router.get('/inbound-paid', checkAuthorizationAdmin, getAllInBoundPaid);
-    router.get('/inbound-cancle', checkAuthorizationAdmin, getAllInBoundCancle);
     app.use("/api/inventory",router);
 }
 
